b890632c4f336a9fdc259059f24fd2f07559ca69
Stanhoucke <stanhoucke@gmail.com>
2021-02-19 14:42:54 +0000

    shop passes attribute tests

--- src/main/java/shop/Shop.java
+++ src/main/java/shop/Shop.java
@@ -1,4 +1,33 @@
 package shop;
 
+import behaviours.ISell;
+
+import java.util.ArrayList;
+
 public class Shop {
+
+    private String name;
+    private ArrayList<ISell> stock;
+
+    public Shop(String name) {
+        this.name = name;
+        this.stock = new ArrayList<ISell>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<ISell> getStock() {
+        return stock;
+    }
+
+    public void setStock(ArrayList<ISell> stock) {
+        this.stock = stock;
+    }
+
 }
--- src/test/java/shop/ShopTest.java
+++ src/test/java/shop/ShopTest.java
@@ -1,4 +1,30 @@
 package shop;
 
+import behaviours.ISell;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
 public class ShopTest {
+
+    Shop shop;
+
+    @Before
+    public void before(){
+        shop = new Shop ("Lord of the Strings");
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Lord of the Strings", shop.getName());
+    }
+
+    @Test
+    public void hasStock(){
+        assertEquals(new ArrayList<ISell>(), shop.getStock());
+    }
+
 }
