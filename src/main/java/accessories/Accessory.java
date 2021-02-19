1493eac999e45da5354115177fb2b510c1c2be89
Stanhoucke <stanhoucke@gmail.com>
2021-02-19 14:14:36 +0000

    accessory passes attribute tests

---
+++ src/main/java/accessories/Accessory.java
@@ -0,0 +1,37 @@
+package accessories;
+
+public class Accessory {
+    private String description;
+    private double buyPrice;
+    private double sellPrice;
+
+    public Accessory(String description, double buyPrice, double sellPrice) {
+        this.description = description;
+        this.buyPrice = buyPrice;
+        this.sellPrice = sellPrice;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getBuyPrice() {
+        return buyPrice;
+    }
+
+    public void setBuyPrice(double buyPrice) {
+        this.buyPrice = buyPrice;
+    }
+
+    public double getSellPrice() {
+        return sellPrice;
+    }
+
+    public void setSellPrice(double sellPrice) {
+        this.sellPrice = sellPrice;
+    }
+}
---
+++ src/test/java/accessories/AccessoryTest.java
@@ -0,0 +1,32 @@
+package accessories;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class AccessoryTest {
+
+    Accessory accessory;
+
+    @Before
+    public void before(){
+        accessory = new Accessory("guitar strings", 3.00, 6.00);
+    }
+
+    @Test
+    public void hasDescription() {
+        assertEquals("guitar strings", accessory.getDescription());
+    }
+
+    @Test
+    public void hasBuyPrice() {
+        assertEquals(3.00, accessory.getBuyPrice(), 0.0);
+    }
+
+    @Test
+    public void hasSellPrice() {
+        assertEquals(6.00, accessory.getSellPrice(), 0.0);
+    }
+}
